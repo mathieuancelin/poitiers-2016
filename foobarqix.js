7cf6cee49fc162e65fc148da7a2076d2ff37b509
Mathieu <mathieu.ancelin@gmail.com>
2016-01-07 09:40:08 +0100

    Fix

--- foobarqix.js
+++ foobarqix.js
@@ -1,5 +1,8 @@
-const chars = ['', '', '', 'Foo', '', 'Bar', '', 'Qix', '', ''];
+'use strict';
+
 const ul = document.createElement('ul');
+
+const chars = ['', '', '', 'Foo', '', 'Bar', '', 'Qix', '', ''];
 for (let i = 1; i < 101; i++) {
   let result = '';
   if (i % 3 === 0) result += 'Foo';
@@ -8,10 +11,11 @@ for (let i = 1; i < 101; i++) {
   result += [].map.call(`${i}`, c => chars[c]).join('');
   const printableResult = result.length === 0 ? i : result;
   const value = `${i} \u21D2 ${printableResult}`;
+  console.log(value);
+  
   const li = document.createElement('li');
   const text = document.createTextNode(value);
   li.appendChild(text);
   ul.appendChild(li);
-  console.log(value);
 }
 document.getElementById('app').appendChild(ul);
