4f73def8abfe5ec5d0d025514f493ebf218e10f1
Mathieu <mathieu.ancelin@gmail.com>
2016-01-07 09:38:28 +0100

    Add foobarqix example

---
+++ foobarqix.js
@@ -0,0 +1,17 @@
+const chars = ['', '', '', 'Foo', '', 'Bar', '', 'Qix', '', ''];
+const ul = document.createElement('ul');
+for (let i = 1; i < 101; i++) {
+  let result = '';
+  if (i % 3 === 0) result += 'Foo';
+  if (i % 5 === 0) result += 'Bar';
+  if (i % 7 === 0) result += 'Qix';
+  result += [].map.call(`${i}`, c => chars[c]).join('');
+  const printableResult = result.length === 0 ? i : result;
+  const value = `${i} \u21D2 ${printableResult}`;
+  const li = document.createElement('li');
+  const text = document.createTextNode(value);
+  li.appendChild(text);
+  ul.appendChild(li);
+  console.log(value);
+}
+document.getElementById('app').appendChild(ul);
